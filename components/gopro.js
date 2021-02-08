6c416534fd013c3e694405df1b4ca8e73ee01888
Ronald Langeveld <ronaldlangeveld@Ronalds-MacBook-Pro.local>
2021-02-08 09:23:09 +0200

    change free results to 7

--- components/gopro.js
+++ components/gopro.js
@@ -6,7 +6,7 @@ const GoPro = () => {
         <div className="notification is-link">
         <div className="content is-medium has-text-centered">
           <p>Thank you for using <strong>⚡ ISP Logger</strong>.</p>
-          <p> The <strong>free</strong> version is limited to the last 48 results.</p>
+          <p> The <strong>free</strong> version is limited to the last 7 results.</p>
           <p>To see all your results and export results, use the date filter, remove this banner and much more, please support us by upgrading to a PRO account</p>
         </div>
         <div className="has-text-centered">
