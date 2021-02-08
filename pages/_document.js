458c79cf6aa6fbb00c6b099115f931c3bc067aaa
Ronald Langeveld <ronaldlangeveld@Ronalds-MacBook-Pro.local>
2021-02-08 21:36:41 +0200

    latest

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-ND6ZBGSSEQ"></script>
 <script type="text/javascript" src="/gtag.js">
+<script async src="/crisp.js" type="text/javascript"></script>
 
 </script>
                 </Head>
@@ -23,6 +24,7 @@ class MyDocument extends Document {
                     <NextScript />
                     <script src="https://cdn.paddle.com/paddle/paddle.js"></script> 
                     <script type="text/javascript" src="/paddle_init.js"></script>
+                
                 </body>
             </Html>
         )
---
+++ public/crisp.js
@@ -0,0 +1,1 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="b4e51207-4d33-4282-a6a6-78283f2cd78e";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
